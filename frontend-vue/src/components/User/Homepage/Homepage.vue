0cdcc5bf57b3f52bd0ad87100645a3b922c06e27
LeTungEm <em.le.tung2103@gmail.com>
2023-03-04 10:26:38 +0700

    fix news layout

--- frontend-vue/src/components/User/Homepage/Homepage.vue
+++ frontend-vue/src/components/User/Homepage/Homepage.vue
@@ -109,7 +109,7 @@
             using
           </p>
         </div>
-        <div class="grid grid-cols-1 lg:grid-cols-3 w-11/12 md:w-7/12 mx-auto gap-10">
+        <div class="grid grid-cols-1 lg:grid-cols-3 w-11/12 md:w-10/12 mx-auto gap-10">
           <div v-for="page in pages" v-on:load="countPages += 1" :key="page.blogId"
             class="shadow-lg border rounded-t-2xl">
             <div v-if="page.blogId <= (pages[2].blogId)">
