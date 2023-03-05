66f45e8643a5c858aae0d7a693360a906e9645e3
LeTungEm <em.le.tung2103@gmail.com>
2023-03-05 13:35:06 +0700

    create AboutItem component

--- frontend-vue/src/components/User/About/About.vue
+++ frontend-vue/src/components/User/About/About.vue
@@ -6,6 +6,11 @@
             :bannerName="'categories'" 
             :shopName="'SHION HOUSE'" 
             :menu="'About US'"/>
+            <AboutItem 
+                :title="'Stablished Sinced 1975'"
+                :imageLink="'balohs1.png'"
+                :content="'But nothing the copy sae they abused her for their'"
+                />
             <div class="md:flex px-4 lg:px-8 xl:px-36 my-24 wrap max-w-max mx-auto">
                 <div class="">
                     <img src="https://themewagon.github.io/minishop/images/about.jpg" alt="" />
@@ -24,30 +29,12 @@
                     </p>
                 </div>
             </div>
-            <div class="md:flex px-4 lg:px-8 xl:px-36 my-24 wrap max-w-max mx-auto">
-                <div class="">
-                    <img src="https://themewagon.github.io/minishop/images/about.jpg" alt="" />
-                </div>
-                <div class="px-8 text-md leading-8 my-6">
-                    <h1 class="text-2xl pb-6 font-bold">Stablished Sinced 1975</h1>
-                    <p class="mb-2">
-                        But nothing the copy said could convince her and so it didn’t take
-                        long until a few insidious Copy Writers ambushed her, made her drunk
-                        with Longe and Parole and dragged her into their agency, where they
-                        abused her for their.
-                    </p>
-                    <p>
-                        But nothing the copy said could convince her and so it didn’t take
-                        long until a few insidious Copy Writers ambushed her.
-                    </p>
-                    <router-link class="link" to="/Shop">
-                        <p
-                            class="mt-12 w-fit text-center text-xl border px-8 mx-auto cursor-pointer duration-300 bg-red-500 text-white py-3 hover:bg-red-600 uppercase rounded-full">
-                            Shop now
-                        </p>
-                    </router-link>
-                </div>
-            </div>
+            <router-link class="link" to="/Shop">
+                <p
+                    class="mt-12 w-fit text-center text-xl border px-8 mx-auto cursor-pointer duration-300 bg-red-500 text-white py-3 hover:bg-red-600 uppercase rounded-full">
+                    Shop now
+                </p>
+            </router-link>
             <div class="py-12 bg-slate-100">
                 <div class="w-10/12 mx-auto">
                     <h1 class="text-6xl text-red-500 font-bold leading-none mb-7 text-center border-b pb-6">
@@ -86,6 +73,7 @@
 import Header from '../Layout/Header.vue'
 import Footer from '../Layout/Footer.vue'
 import Banner from '../Layout/Banner.vue';
+import AboutItem from './AboutItem.vue';
 
 export default {
     name: "AboutUs",
@@ -93,6 +81,7 @@ export default {
         Header,
         Footer,
         Banner,
+        AboutItem,
     },
 }
 </script>
---
+++ frontend-vue/src/components/User/About/AboutItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="md:flex px-4 lg:px-8 xl:px-36 my-24 wrap max-w-max mx-auto">
+        <div class="">
+            <img :src="require('../../../assets/images/balo/'+ imageLink)" alt="" />
+        </div>
+        <div class="px-8 text-md leading-8 my-6">
+            <h1 class="text-2xl pb-6 font-bold">{{title}}</h1>
+            <p class="mb-2">
+                {{content}}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AboutItem',
+    props:{
+        imageLink: String,
+        title: String,
+        content: String,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
