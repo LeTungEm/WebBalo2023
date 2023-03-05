6581bbe0fe1c086edbb52dcb2f3acb7d1d6f3997
LeTungEm <em.le.tung2103@gmail.com>
2023-03-05 14:39:39 +0700

    fix About page layout

--- frontend-vue/src/components/User/About/AboutItem.vue
+++ frontend-vue/src/components/User/About/AboutItem.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="md:flex px-4 lg:px-8 xl:px-36 my-24 wrap max-w-max mx-auto">
-        <div class="">
-            <img :src="require('../../../assets/images/balo/'+ imageLink)" alt="" />
+    <div class="grid grid-cols-5 px-4 lg:px-8 xl:px-36 my-24 wrap max-w-max mx-auto">
+        <div class="col-span-5 md:col-span-2 w-full">
+            <img class="w-fit mx-auto h-full object-cover" :src="require('../../../assets/images/about/'+ imageLink)" alt="image" />
         </div>
-        <div class="px-8 text-md leading-8 my-6">
+        <div class="col-span-5 md:col-span-3 px-8 text-md leading-8 my-6">
             <h1 class="text-2xl pb-6 font-bold">{{title}}</h1>
-            <p class="mb-2">
+            <p>
                 {{content}}
             </p>
         </div>
