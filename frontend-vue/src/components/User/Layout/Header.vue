bb32e68842074cd770b3d45071d3ff0c297eb5d6
Phongnehe <phongnebe@gmail.com>
2023-03-13 17:15:39 +0700

    hieu ung lan2

--- frontend-vue/src/components/User/Layout/Header.vue
+++ frontend-vue/src/components/User/Layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
   
-  <header class="sticky w-full top-0 z-20 bg-white"  data-aos="fade-down" data-aos-duration="1000" data-aos-delay='500'>
+  <header class="sticky w-full top-0 z-20 bg-white" >
     <div class="flex justify-between p-3 border-b shadow-lg">
         <div class="flex justify-between items-center w-full">
           <router-link to="/">
