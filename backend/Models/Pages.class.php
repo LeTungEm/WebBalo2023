9a193b749679ada2a6e080e5ca781675b805eae2
Phongnehe <phongnebe@gmail.com>
2023-03-23 11:33:19 +0700

    PHONG

---
+++ backend/Models/Pages.class.php
@@ -0,0 +1,32 @@
+<?php
+    class Pages extends Db
+    {
+        function getAll(){
+            $sql = 'select * from pages';
+            return $this->select($sql);
+        }
+
+        function getByID($blogId){
+            $sql = 'select * from pages where blogId = ?';
+            $data = $this->select($sql, array($blogId));
+            return ($data != null)?$data[0]:null;
+        }
+
+     
+
+        function insertPages($blogName, $content, $image, $createDate, $author, $published){
+            $sql = "INSERT INTO `pages`( `blogName`, `content`, `image`, `createDate`, `author`, `published`) VALUES (?,?,?,?,?,?)";
+            return $this->insert($sql, array($blogName, $content, $image, $createDate, $author, $published));
+        }
+
+        function deletePages($blogId){
+            $sql = 'DELETE FROM `pages` WHERE blogId = ?';
+            return $this->select($sql, array($blogId));
+        }
+
+        function updatePages($blogName, $content, $image, $createDate, $author, $published){
+            $sql = 'UPDATE `pages` SET `blogName`= ?,`content`= ?,`createDate`= ?,`image`= ?,`author`= ?,`published`= ? WHERE blogId = ?';
+            return $this->update($sql, array($blogName, $content, $image, $createDate, $author, $published));
+        }
+    }
+?>
--- frontend-vue/src/service/RoleService.js
+++ frontend-vue/src/service/RoleService.js
@@ -11,14 +11,42 @@ const PRODUCT_API_URL =
           });
     }
 
-    getByID(catID){
+    getByID(roleID){
         return axios.get(`${PRODUCT_API_URL}`, {
             params: {
                 action: "getByID",
-                catID: catID,
+                roleID: roleID,
             },
           });
     }
+    insertRole(roleName){
+      return axios.get(`${PRODUCT_API_URL}`, {
+        params: {
+            action: "insertRole",
+            roleName: roleName
+        },
+      });
+    }
+
+    deleteRole(roleID){
+      return axios.get(`${PRODUCT_API_URL}`, {
+        params: {
+            action: "deleteRole",
+            roleID: roleID,
+        },
+      });
+    }
+    updateRole(roleName, roleID){
+      return axios.get(`${PRODUCT_API_URL}`, {
+        params: {
+            action: "updateRole",
+            roleID: roleID,
+            roleName: roleName
+          
+        },
+      });
+    }
+
   }
 
 export default new RolesService();
